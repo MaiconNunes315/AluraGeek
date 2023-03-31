d4c5a4e3c6f6fe5ff1bb7c6eb029001460e6f636
Maicon Nunes <mnunes315@hotmail.com>
2023-03-31 18:16:18 -0300

    Header finalizado

---
+++ src/scripts/script.js
@@ -0,0 +1,35 @@
+// const searchButton = document.querySelector('.search');
+// const searchContainer = document.querySelector('.header__search');
+// const input = document.querySelector('input');
+// const header = document.querySelector("header");
+// const body = document.querySelector("body");
+
+// function inputVisible() {
+    
+//     if (innerWidth <= 768) {
+        
+    
+//             input.style.display = "block";
+//             input.style.opacity = "1"
+//             input.style.transition = "opacity 2s"
+//             searchContainer.style.background = "#F5F5F5"
+//             searchContainer.style.width = "50%";
+//             header.style.justifyContent = "center";
+//             header.style.gap = "15px";
+        
+//     }
+
+// }
+
+// addEventListener("resize", () => {
+//     searchContainer.addEventListener("click", inputVisible)
+// })
+
+
+
+// searchContainer.addEventListener("click", inputVisible)
+
+
+
+
+
