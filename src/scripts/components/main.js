66a2a5762f7d1d85cc34073753e253be6a5aff92
Maicon Nunes <mnunes315@hotmail.com>
2023-04-02 18:20:10 -0300

    Cards de produtos finalizado

---
+++ src/scripts/components/main.js
@@ -0,0 +1,58 @@
+
+export function sectionComponent(title) {
+    
+    const section = document.createElement("section");
+    section.classList.add("products");
+
+    const productsInfo = document.createElement("div");
+    productsInfo.classList.add("products__info");
+    section.appendChild(productsInfo);
+
+    const h2 = document.createElement("h2");
+    h2.innerText = title;
+    productsInfo.appendChild(h2);
+
+    const allProducts = document.createElement("a");
+    allProducts.setAttribute("href", "#");
+    allProducts.innerText = "Ver tudo";
+    productsInfo.appendChild(allProducts);
+
+    const arrow = document.createElement("svg");
+    allProducts.appendChild(arrow);
+
+    arrow.innerHTML = `<svg viewBox="0 -4.5 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>arrow_right [#346]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-300.000000, -6643.000000)" fill="#2A7AE4"> <g id="icons" transform="translate(56.000000, 160.000000)"> <polygon id="arrow_right-[#346]" points="264 6488.26683 258.343 6483 256.929 6484.21678 260.172 6487.2264 244 6487.2264 244 6489.18481 260.172 6489.18481 256.929 6492.53046 258.343 6494"> </polygon> </g> </g> </g> </g>
+    </svg>` 
+
+    const productsCards = document.createElement("div");
+    productsCards.classList.add("products__cards");
+    section.appendChild(productsCards);
+    return section  
+    
+}
+
+
+export function cardProductComponent(insertCards, img, title, price) {
+    
+
+    const productsCard = document.createElement("div");
+    productsCard.classList.add("products__card");
+    insertCards.appendChild(productsCard);
+
+    const imgProduct = document.createElement("img");
+    imgProduct.setAttribute("src", `./src/images/${img}.png`);
+    imgProduct.setAttribute("alt", "Imagem do produto");
+    productsCard.appendChild(imgProduct);
+
+    const titleProduct = document.createElement("h3");
+    titleProduct.innerText = title;
+    productsCard.appendChild(titleProduct);
+
+    const priceProduct = document.createElement("span");
+    priceProduct.innerText = `R$ ${price}`;
+    productsCard.appendChild(priceProduct);
+
+    const viewProduct = document.createElement("a");
+    viewProduct.setAttribute("href", "#");
+    viewProduct.innerText = "Ver produto";
+    productsCard.appendChild(viewProduct);
+}
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,35 +1,37 @@
-// const searchButton = document.querySelector('.search');
-// const searchContainer = document.querySelector('.header__search');
-// const input = document.querySelector('input');
-// const header = document.querySelector("header");
-// const body = document.querySelector("body");
+import { sectionComponent } from "./components/main.js";
+import { cardProductComponent } from "./components/main.js";
 
-// function inputVisible() {
+
+(() => {
+    const main = document.querySelector("main");
     
-//     if (innerWidth <= 768) {
-        
-    
-//             input.style.display = "block";
-//             input.style.opacity = "1"
-//             input.style.transition = "opacity 2s"
-//             searchContainer.style.background = "#F5F5F5"
-//             searchContainer.style.width = "50%";
-//             header.style.justifyContent = "center";
-//             header.style.gap = "15px";
-        
-//     }
-
-// }
-
-// addEventListener("resize", () => {
-//     searchContainer.addEventListener("click", inputVisible)
-// })
+    main.appendChild(sectionComponent("Star Wars"));
+    main.appendChild(sectionComponent("Console"));
+    main.appendChild(sectionComponent("Diversos"));
+    const productsCard = document.querySelectorAll(".products__cards");
 
 
+    cardProductComponent( productsCard[0], "star1", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[0], "star2", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[0], "star3", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[0],  "star4", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[0], "star5", "Produto XYZ", "60,00")
+    cardProductComponent(productsCard[0], "star6", "Produto XYZ", "60,00")
+   
 
-// searchContainer.addEventListener("click", inputVisible)
+    cardProductComponent( productsCard[1], "Consoles1", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[1], "Consoles2", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[1], "Consoles3", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[1],  "Consoles4", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[1], "Consoles5", "Produto XYZ", "60,00")
+    cardProductComponent(productsCard[1], "Consoles6", "Produto XYZ", "60,00")
 
+    cardProductComponent( productsCard[2], "diversos1", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[2], "diversos2", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[2], "diversos3", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[2],  "diversos4", "Produto XYZ", "60,00")
+    cardProductComponent( productsCard[2], "diversos5", "Produto XYZ", "60,00")
+    cardProductComponent(productsCard[2], "diversos6", "Produto XYZ", "60,00")
 
-
-
-
+  
+})()
