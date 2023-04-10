984dfc9ffcfda965fd46d3e504afad0885dee6d6
Maicon Nunes <mnunes315@hotmail.com>
2023-04-10 20:44:28 -0300

    Edição de produtos

--- src/scripts/components/main.js
+++ src/scripts/components/main.js
@@ -1,6 +1,6 @@
 
 export function sectionComponent(title) {
-    
+
     const section = document.createElement("section");
     section.classList.add("products");
 
@@ -13,7 +13,7 @@ export function sectionComponent(title) {
     productsInfo.appendChild(h2);
 
     const allProducts = document.createElement("a");
-    allProducts.setAttribute("href", "#");
+    allProducts.setAttribute("href", "products.html");
     allProducts.innerText = "Ver tudo";
     productsInfo.appendChild(allProducts);
 
@@ -21,18 +21,18 @@ export function sectionComponent(title) {
     allProducts.appendChild(arrow);
 
     arrow.innerHTML = `<svg viewBox="0 -4.5 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>arrow_right [#346]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-300.000000, -6643.000000)" fill="#2A7AE4"> <g id="icons" transform="translate(56.000000, 160.000000)"> <polygon id="arrow_right-[#346]" points="264 6488.26683 258.343 6483 256.929 6484.21678 260.172 6487.2264 244 6487.2264 244 6489.18481 260.172 6489.18481 256.929 6492.53046 258.343 6494"> </polygon> </g> </g> </g> </g>
-    </svg>` 
+    </svg>`
 
     const productsCards = document.createElement("div");
     productsCards.classList.add("products__cards");
     section.appendChild(productsCards);
-    return section  
-    
+    return section
+
 }
 
 
 export function cardProductComponent(insertCards, img, title, price) {
-    
+
 
     const productsCard = document.createElement("div");
     productsCard.classList.add("products__card");
